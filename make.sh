4c2b70a8ba0d9e4582f1e4d9d85040c8968e5f8c
Karan Thukral <karanthukral@gmail.com>
2015-07-16 21:36:32 -0400

    Deleted zprezto folder

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc gitconfig tmux.conf zpreztorc zprofile zprezto"    # list of files/folders to symlink in homedir
+files="vimrc zshrc gitconfig tmux.conf zpreztorc zprofile"    # list of files/folders to symlink in homedir
 
 ##########
 
